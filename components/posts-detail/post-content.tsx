9dd40050e587aacdaabcc990f8c7cecf2405c6c2
moviefan322 <philman202@yahoo.com>
2023-06-11 15:22:10 -0400

    adds custom renderers to ReactMarkdown component

--- components/posts-detail/post-content.tsx
+++ components/posts-detail/post-content.tsx
@@ -3,19 +3,54 @@ import PostHeader from "./post-header";
 import { Post } from "../../types/types";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 interface PostContentProps {
   post: Post;
 }
 
 function PostContent({ post }: PostContentProps) {
-  const { title, image, content } = post;
+  const { title, image, content, slug } = post;
 
   const imagePath = `/images/posts/${image}`;
+
+  const customRenderers = {
+    // img(image: any) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${slug}/${image.src}`}
+    //       alt={image.alt}
+    //       height={300}
+    //       width={600}
+    //     />
+    //   );
+    // },
+
+    p(paragraph: any) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              height={300}
+              width={600}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 }
