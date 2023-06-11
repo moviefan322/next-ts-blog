5dd49d416796141e5e7fd9fd3fbfc7cf8a2534d0
moviefan322 <philman202@yahoo.com>
2023-06-11 15:58:16 -0400

    adds api route to handle contact message

---
+++ pages/api/contact.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    console.log(newMessage);
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", outgoingMessage: newMessage });
+  }
+}
+
+export default handler;
