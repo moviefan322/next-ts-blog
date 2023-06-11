b4224e053e4b6156a51927db4315486012cc78d5
moviefan322 <philman202@yahoo.com>
2023-06-11 17:40:15 -0400

    adds submit handler to contact form

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const messageInputRef = useRef<HTMLTextAreaElement>(null);
+
+  async function sendMessageHandler(event: React.FormEvent) {
+    event.preventDefault();
+    // optional: add validation
+
+    const enteredEmail = emailInputRef.current!.value;
+    const enteredName = nameInputRef.current!.value;
+    const enteredMessage = messageInputRef.current!.value;
+
+    // fetch API
+    const res = await fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  }
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required ref={emailInputRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required ref={nameInputRef} />
           </div>
         </div>
         <div className={classes.control}>
-            <label htmlFor="message">Your Message</label>
-            <textarea id="message" rows={5} required />
-          </div>
-          <div className={classes.actions}>
-            <button>Send Message</button>
-          </div>
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" rows={5} required ref={messageInputRef} />
+        </div>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
       </form>
     </section>
   );
