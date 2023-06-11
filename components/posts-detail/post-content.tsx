8af40ebcddf9362a024800ae4b15b080cb01644c
moviefan322 <philman202@yahoo.com>
2023-06-11 14:08:30 -0400

    implements ReactMarkdown to render md as jsx

--- components/posts-detail/post-content.tsx
+++ components/posts-detail/post-content.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import PostHeader from "./post-header";
 import { Post } from "../../types/types";
-import { DUMMY_POSTS } from "@/utils/dummyData";
 import classes from "./post-content.module.css";
+import ReactMarkdown from "react-markdown";
 
 interface PostContentProps {
   post: Post;
 }
 
 function PostContent({ post }: PostContentProps) {
-  const { title, image } = post;
+  const { title, image, content } = post;
 
   const imagePath = `/images/posts/${image}`;
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      {DUMMY_POSTS[0].content}
+      <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
 }
