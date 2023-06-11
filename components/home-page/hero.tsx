906786a2ec9f2307cca0d3bbd2ea656f40576f51
moviefan322 <philman202@yahoo.com>
2023-06-10 20:54:20 -0400

    adds content to hero component

--- components/home-page/hero.tsx
+++ components/home-page/hero.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Image from "next/image";
 import classes from "./hero.module.css";
 
 function Hero() {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/topcat.png"
+          alt="an image of Phil"
+          width={300}
+          height={326}
+        />
+      </div>
+      <h1>Hi, I&#39m Phil</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+      </p>
+    </section>
+  );
 }
 
 export default Hero;
