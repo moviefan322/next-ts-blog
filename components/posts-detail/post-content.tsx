25af1a1fda90b39ebe490dea24dc46bf91c88bb6
moviefan322 <philman202@yahoo.com>
2023-06-11 23:50:54 -0400

    optimizes Prisma imports to make a smaller build

--- components/posts-detail/post-content.tsx
+++ components/posts-detail/post-content.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import PostHeader from "./post-header";
 import { Post } from "../../types/types";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 interface PostContentProps {
   post: Post;
